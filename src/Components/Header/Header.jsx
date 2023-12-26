2b4f3d9e3634b0bee80d3e831d63b4d603f0841c
Rohit pareek <132699909+RohitPareekWebDev@users.noreply.github.com>
2023-12-26 23:30:57 +0530

    set for mobile users

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './Header.css';
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { GiHamburgerMenu } from "react-icons/gi";
 
 // Define a functional component named 'Header'
@@ -8,11 +8,11 @@ function Header() {
     const [showMediaIcons, setShowMediaIcons] = useState(false);
 
     // Define a custom CSS class for NavLink components
-    const NavLinkCss = ({ isActive }) => `${isActive ? "text-blue-500" : "text-black"} p-2 mx-5 hover:text-blue-500`
+    const NavLinkCss = ({ isActive }) => `${isActive ? "text-blue-500" : "text-black"} font-bold text-2xl p-5 hover:text-blue-500`
 
     return (
         // Create a navigation bar using the 'nav' element
-        <nav className='flex p-2 justify-between shadow-lg '>
+        <nav className='flex p-2 justify-between shadow-lg'>
             <span id='logo' className=" mt-2 bg-gradient-to-r from-blue-500 to-black text-transparent bg-clip-text text-4xl font-bold ">
                 {/* Add AlmaBetter logo as a link to the '/resumetemp' route */}
                 <NavLink to="/" >
@@ -20,23 +20,23 @@ function Header() {
                 </NavLink>
             </span>
 
-            <div className={showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}>
-
-                <NavLink to="/" className={NavLinkCss}> Resume Templates</NavLink>
-
-                <NavLink to="/myresume" className={NavLinkCss}>My Resume</NavLink>
-
-                <NavLink to="/about" className={NavLinkCss}>About Us</NavLink>
-            </div>
-
             <div className="social-media">
 
                 {/* hamburget menu start  */}
                 <div className="hamburger-menu">
-                    <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                    <Link href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
                         <GiHamburgerMenu />
-                    </a>
+                    </Link>
                 </div>
+            </div>
+
+            <div className={showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}>
+
+                <NavLink to="/" className={`${NavLinkCss}`}> Resume Templates</NavLink>
+
+                <NavLink to="/myresume" className={NavLinkCss}>My Resume</NavLink>
+
+                <NavLink to="/about" className={NavLinkCss}>About Us</NavLink>
             </div>
         </nav>
     )
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,50 +1,41 @@
-import React, { useState } from 'react'
-import { templateImages } from '../ImagesFile/images'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import "./HomePage.css";
+import { templateImages } from '../ImagesFile/images';
+import { NavLink } from 'react-router-dom';
+
 function ResumeTemp() {
-    const [mouseOver, setMouseOver] = useState("mouseNotOver")
+    const [mouseOver, setMouseOver] = useState("mouseNotOver");
 
     return (
-        <div className='container flex flex-col items-center justify-center h-auto  mx-auto'>
-            <div >
-                <h1 className='m-10  p-10 font-bold text-2xl flex justify-center '>Select a Template to get started!</h1>
+        <div>
+            <div>
+                <h1 className='m-10 p-10 font-bold text-2xl flex justify-center '>Select a Template to get started!</h1>
             </div>
-            <div className=' flex justify-between items-center'>
-                {
-                    templateImages && templateImages.map((template) => {
-                        return (
-                            <div>
-                                <div className=' text-blue-500 flex justify-center font-bold mb-5 text-xl'><h2>{template.name}</h2></div>
+            <div id="one" className='container flex flex-col items-center justify-center h-auto mx-auto'>
+                <div className='flex flex-wrap justify-center'>
+                    {templateImages &&
+                        templateImages.map((template, index) => (
+                            <div id='two' key={template.name} className={`mb-5 ${index > 0 ? 'ml-2' : ''} sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4`}>
+                                <div className='text-blue-500 flex justify-center font-bold mb-5 text-xl'><h2>{template.name}</h2></div>
                                 <div
-                                    onMouseOver={() => {
-                                        //this function allows us to display 'Use Template'button on the top of the targeted template, when the user hovers over it by setting state's value to the targeted template name.//
-                                        setMouseOver(template.name)
-                                    }}
-                                    onMouseOut={() => {
-                                        //this function allows us to hide 'Use Template' button when the user moves out from the particular template//
-                                        setMouseOver('MouseIsNotOver')
-                                    }}
-                                    className="mx-7 flex justify-between h-96 w-80 ml-2" key={template.name}>
-                                    <img className={`transform transition-transform ${mouseOver === template.name ? 'scale-110' : 'scale-100'
-                                        }`} src={template.imageSource} alt="error" />
-                                    {
-                                        mouseOver === template.name ?
-                                            <div className='absolute mx-7 flex justify-between h-96 w-80 ml-2'>
-                                                <div className='absolute inset-0 flex items-center justify-center'>
-                                                    <NavLink to="/detailfilling/personalinfo" ><button id='tempUseBtn' className='bg-blue-500 h-10 px-2 rounded-xl font-bold text-white'>Use Template</button></NavLink>
-                                                </div>
+                                    onMouseOver={() => setMouseOver(template.name)}
+                                    onMouseOut={() => setMouseOver('MouseIsNotOver')}
+                                    className={`mx-7 flex justify-between h-96 w-80 ${mouseOver === template.name ? 'scale-110' : 'scale-100'}`}>
+                                    <img className="transform transition-transform" src={template.imageSource} alt="error" />
+                                    {mouseOver === template.name ? (
+                                        <div className='absolute mx-7 flex justify-between h-96 w-80'>
+                                            <div className='absolute inset-0 flex items-center justify-center'>
+                                                <NavLink to="/detailfilling/personalinfo" ><button id='tempUseBtn' className='bg-blue-500 h-10 px-2 rounded-xl font-bold text-white'>Use Template</button></NavLink>
                                             </div>
-                                            : null
-                                    }
+                                        </div>
+                                    ) : null}
                                 </div>
                             </div>
-                        )
-                    })
-                }
+                        ))}
+                </div>
             </div>
-
-        </div >
-    )
+        </div>
+    );
 }
 
-export default ResumeTemp
+export default ResumeTemp;
