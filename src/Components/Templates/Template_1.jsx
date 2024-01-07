7670dcfcabb9ccd9e45d8ef54bc393b633095c16
rohit <vyas.rohitt@gmail.com>
2024-01-07 16:02:32 +0530

    fixing errors

--- src/Components/Templates/Template_1.jsx
+++ src/Components/Templates/Template_1.jsx
@@ -1,59 +1,76 @@
-import React from 'react';
+import React from "react";
 
 function Template_1() {
-    return (
-        <div className='min-h-screen flex items-center justify-center  '>
-            <div className='w-1/2 h-screen m-5 p-5 shadow-black shadow-lg'>
-                <div className=' h-1/4 flex justify-between items-center border-b-8 outline-none border-blue-400'>
-                    <div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" width={'200px'} alt="" />
-                    </div>
+  return (
+    <div className="min-h-screen flex items-center justify-center  ">
+      <div className="w-1/2 h-screen m-5 p-5 shadow-black shadow-lg">
+        <div className=" h-1/4 flex justify-between items-center border-b-8 outline-none border-blue-400">
+          <div>
+            <img src="" width={"200px"} alt="" />
+          </div>
 
-                    <div>
-                        <h1 className='font-bold text-5xl text-blue-400'>ROHIT PAREEK</h1>
-                        <div className='flex justify-center'>
-                            <h3 className='font-bold'>Job Title</h3>
-                        </div>
-                    </div>
-                    <div className='w-52'>
-                        sereenawislon@gamil.com
-                        43479857409 <br />
-                        Apartment 4, Wall Street, Lane
-                        Journal, jummaidnagar, Karnataka,
-                        686674
-                    </div>
-                </div>
-
-                <div className='p-5 border-b-4 outline-none border-blue-400'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat exercitationem laboriosam o Magnam animi molestias incidunt dolore expedita quasi! Animi id a illum alias, accusantium sequi necessitatibus consectetur numquam, corrupti voluptatibus ex, vero modi nulla.</p>
-                </div>
-
-                <div className='flex p-5 border-b-4 outline-none border-blue-400 '>
-                    <div className='w-1/4 font-bold text-blue-400'><h2>Professional Experience</h2></div>
-                    <div className='grid w-3/4 '>
-                        <h3 className='font-bold text-blue-400'>Job Title</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium quisquam explicabo magnam non totam alias sunt molestias aliquid a dolor?</p>
-                    </div>
-                </div>
-
-                <div className='flex p-5 border-b-4 outline-none border-blue-400'>
-                    <div className='w-1/4 font-bold text-blue-400'><h2>Education</h2></div>
-                    <div className='grid w-3/4 '>
-                        <h3 className='font-bold text-blue-400'>Bachelor of Computer science</h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium quisquam explicabo magnam non totam alias sunt molestias aliquid a dolor?</p>
-                    </div>
-                </div>
-
-                <div className='flex p-5  outline-none '>
-                    <div className='w-1/4  font-bold text-blue-400'><h2>Key Skills</h2></div>
-                    <div className='grid w-3/4 '>
-
-                    </div>
-                </div>
-
+          <div>
+            <h1 className="font-bold text-5xl text-blue-400">ROHIT PAREEK</h1>
+            <div className="flex justify-center">
+              <h3 className="font-bold">Job Title</h3>
             </div>
+          </div>
+          <div className="w-52">
+            sereenawislon@gamil.com 43479857409 <br />
+            Apartment 4, Wall Street, Lane Journal, jummaidnagar, Karnataka,
+            686674
+          </div>
         </div>
-    );
+
+        <div className="p-5 border-b-4 outline-none border-blue-400">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            exercitationem laboriosam o Magnam animi molestias incidunt dolore
+            expedita quasi! Animi id a illum alias, accusantium sequi
+            necessitatibus consectetur numquam, corrupti voluptatibus ex, vero
+            modi nulla.
+          </p>
+        </div>
+
+        <div className="flex p-5 border-b-4 outline-none border-blue-400 ">
+          <div className="w-1/4 font-bold text-blue-400">
+            <h2>Professional Experience</h2>
+          </div>
+          <div className="grid w-3/4 ">
+            <h3 className="font-bold text-blue-400">Job Title</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Praesentium quisquam explicabo magnam non totam alias sunt
+              molestias aliquid a dolor?
+            </p>
+          </div>
+        </div>
+
+        <div className="flex p-5 border-b-4 outline-none border-blue-400">
+          <div className="w-1/4 font-bold text-blue-400">
+            <h2>Education</h2>
+          </div>
+          <div className="grid w-3/4 ">
+            <h3 className="font-bold text-blue-400">
+              Bachelor of Computer science
+            </h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Praesentium quisquam explicabo magnam non totam alias sunt
+              molestias aliquid a dolor?
+            </p>
+          </div>
+        </div>
+
+        <div className="flex p-5  outline-none ">
+          <div className="w-1/4  font-bold text-blue-400">
+            <h2>Key Skills</h2>
+          </div>
+          <div className="grid w-3/4 "></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Template_1;
--- src/Components/Templates/Template_3.jsx
+++ src/Components/Templates/Template_3.jsx
@@ -1,70 +1,98 @@
-import React from 'react';
+import React from "react";
 
 function Template_3() {
-    return (
-        <div className='min-h-screen flex items-center justify-center'>
-            <div className='w-1/2 h-screen m-5 p-5'>
-                <div className='flex '>
-                    <div className={`w-1/4`} style={{ backgroundColor: '#8a4242' }}>
-                        <div className='p-10'>
-                            <img className='h-30' src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="" />
-                        </div>
-                        <div className='text-white'>
-                            <h2 className='flex justify-center font-bold text-2xl'>ROHIT PAREEK</h2>
-                            <div className='flex justify-center'>
-                                <h3 className='font-bold'>Job Title</h3>
-                            </div>
-                        </div>
-
-                        <div className='mt-5 p-3'>
-                            <label className='bg-white my-2' htmlFor="">Email :</label>
-                            <div className='text-white my-2'>vyasrohit200@gmail.com</div>
-                            <label className='bg-white my-2' htmlFor="">Contact : </label>
-                            <div className='text-white my-2'>7878713945</div>
-                            <label className='bg-white my-2' htmlFor="">Address : </label>
-                            <div className='text-white my-2'>Budh vihar pal village jodhpur</div>
-                        </div>
-                    </div>
-
-                    <div className='p-3 w-3/4' style={{ backgroundColor: "#F8EEBB" }}>
-                        <div>
-                            <p className='border-red-900 border-b-4 pb-5'>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum eius inventore blanditiis ab dolores repellendus nam id modi quisquam similique?
-                            </p>
-                        </div>
-
-                        <div>
-                            <h3 className='mt-3 font-bold text-2xl bg-orange-50 text-red-900'>Professional Experience</h3>
-                            <div className='border-red-900 border-b-4 pb-5'>
-                                <h3 className='font-bold text-xl mt-2'>Job Title</h3>
-                                <h3 className='font-bold mt-2'>Job Title</h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium quisquam explicabo magnam non totam alias sunt molestias aliquid a dolor?</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <h3 className='mt-3 font-bold text-2xl bg-orange-50 text-red-900'>Education</h3>
-                            <div className='border-red-900 border-b-4 pb-5'>
-                                <h3 className='font-bold text-lg mt-2'>Bachelor of Engineering & Technology</h3>
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium quisquam explicabo magnam non totam alias sunt molestias aliquid a dolor?</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <h3 className='mt-3 font-bold text-2xl bg-orange-50 text-red-900'>Key Skills</h3>
-                            <div>
-                                <li>HTML</li>
-                                <li>JAVASCRIPT</li>
-                                <li>CSS</li>
-                                <li>REACT</li>
-                                <li>NEXTJS</li>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-1/2 h-screen m-5 p-5">
+        <div className="flex ">
+          <div className={`w-1/4`} style={{ backgroundColor: "#8a4242" }}>
+            <div className="p-10">
+              <img className="h-30" src="" alt="" />
             </div>
-        </div >
-    );
+            <div className="text-white">
+              <h2 className="flex justify-center font-bold text-2xl">
+                ROHIT PAREEK
+              </h2>
+              <div className="flex justify-center">
+                <h3 className="font-bold">Job Title</h3>
+              </div>
+            </div>
+
+            <div className="mt-5 p-3">
+              <label className="bg-white my-2" htmlFor="">
+                Email :
+              </label>
+              <div className="text-white my-2">vyasrohit200@gmail.com</div>
+              <label className="bg-white my-2" htmlFor="">
+                Contact :{" "}
+              </label>
+              <div className="text-white my-2">7878713945</div>
+              <label className="bg-white my-2" htmlFor="">
+                Address :{" "}
+              </label>
+              <div className="text-white my-2">
+                Budh vihar pal village jodhpur
+              </div>
+            </div>
+          </div>
+
+          <div className="p-3 w-3/4" style={{ backgroundColor: "#F8EEBB" }}>
+            <div>
+              <p className="border-red-900 border-b-4 pb-5">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Laborum eius inventore blanditiis ab dolores repellendus nam id
+                modi quisquam similique?
+              </p>
+            </div>
+
+            <div>
+              <h3 className="mt-3 font-bold text-2xl bg-orange-50 text-red-900">
+                Professional Experience
+              </h3>
+              <div className="border-red-900 border-b-4 pb-5">
+                <h3 className="font-bold text-xl mt-2">Job Title</h3>
+                <h3 className="font-bold mt-2">Job Title</h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Praesentium quisquam explicabo magnam non totam alias sunt
+                  molestias aliquid a dolor?
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <h3 className="mt-3 font-bold text-2xl bg-orange-50 text-red-900">
+                Education
+              </h3>
+              <div className="border-red-900 border-b-4 pb-5">
+                <h3 className="font-bold text-lg mt-2">
+                  Bachelor of Engineering & Technology
+                </h3>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Praesentium quisquam explicabo magnam non totam alias sunt
+                  molestias aliquid a dolor?
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <h3 className="mt-3 font-bold text-2xl bg-orange-50 text-red-900">
+                Key Skills
+              </h3>
+              <div>
+                <li>HTML</li>
+                <li>JAVASCRIPT</li>
+                <li>CSS</li>
+                <li>REACT</li>
+                <li>NEXTJS</li>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Template_3;
