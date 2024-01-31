d6293b3ab8402469a2ebdadfdb8fefc85c72e782
rohit <vyas.rohitt@gmail.com>
2024-01-31 23:51:03 +0530

    updating_2

--- src/Components/Detail/WorkExperience.jsx
+++ src/Components/Detail/WorkExperience.jsx
@@ -1,42 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-import { useDispatch } from "react-redux";
-import { updateWorkEx } from '../../Store/dataStoreSlice'
-
-function WorkExperience() {
-  const [inputEx, setInputEx] = useState({
-=======
 import { updateWorkEx } from "../../Store/dataStoreSlice";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 
 function WorkExperience() {
   const [input, setInput] = useState({
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
     title: "",
     orgName: "",
     startYear: "",
     endYear: "",
   });
-<<<<<<< HEAD
-
   const dispatch = useDispatch();
 
-  const addWorkEx = () => {
-    console.log("form submitted ex");
-    console.table(inputEx);
-    dispatch(updateWorkEx(inputEx));
-    setInputEx({
-=======
-  const dispatch = useDispatch();
-
-  const addWorkEx = () => {
+  const addWorkEx = (e) => {
+    e.preventDefault();
     console.log("form submitted");
     console.table(input);
     dispatch(updateWorkEx(input));
     setInput({
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
       title: "",
       orgName: "",
       startYear: "",
@@ -44,122 +26,120 @@ function WorkExperience() {
     });
   };
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
   let inputCss = "w-full border rounded-lg p-2 shadow-blue-950 shadow-sm";
   let year = yearRange(2000, 2024);
+
   function yearRange(start, end) {
-    //this function  is used to create list of years in a range to display list of options in the 'Select' input field of the form.
     var ans = [];
     for (let i = start; i <= end; i++) {
       ans.push(i);
     }
     return ans;
   }
+
   return (
-    <div className=" flex w-3/4 flex-col p-5 shadow-gray-500 shadow-xl">
-      <h1 className=" text-2xl font font-bold  text-justify mt-10 border-b-2 border-blue-400">
+    <div className="flex w-3/4 flex-col p-5 shadow-gray-500 shadow-xl">
+      <h1 className="text-2xl font-bold text-justify mt-10 border-b-2 border-blue-400">
         Work Experience
       </h1>
+
       <div className="flex justify-center mt-10">
         <form onSubmit={addWorkEx}>
-          <h2 className=" text-xl font font-bold  text-justify mt-10 border-b-2 border-blue-200">
+          <h2 className="text-xl font-bold text-justify mt-10 border-b-2 border-blue-200">
             Experience
           </h2>
 
           <div className="grid grid-cols-2 gap-4 mt-2.5">
             <div>
-              <label className=" text-sm font-bold" htmlFor="title">
+              <label className="text-sm font-bold" htmlFor="jobTitle">
                 Job title
               </label>
               <input
                 className={`${inputCss}`}
-                id="title"
+                id="jobTitle"
                 type="text"
-<<<<<<< HEAD
-                placeholder="Enter Job Title"
-                value={inputEx.title}
-                onChange={(e) => setInputEx({ ...inputEx, title: e.target.value })}
-=======
-                // placeholder="Enter Job Title"
-                value={input}
+                value={input.title}
                 onChange={(e) => {
                   setInput({ ...input, title: e.target.value });
                 }}
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
               />
             </div>
 
             <div>
-              <label className=" text-sm font-bold" htmlFor="Organization Name">
+              <label className="text-sm font-bold" htmlFor="orgName">
                 Organization Name
               </label>
               <input
                 className={`${inputCss}`}
-                id="Organization Name"
+                id="orgName"
                 type="text"
-                placeholder="Enter Organization Name"
+                value={input.orgName}
+                onChange={(e) => {
+                  setInput({ ...input, orgName: e.target.value });
+                }}
               />
             </div>
-            
           </div>
 
           <div className="grid grid-cols-2 gap-4 mt-2.5">
             <div>
-              <label className=" text-sm font-bold" htmlFor="Starting Year ">
+              <label className="text-sm font-bold" htmlFor="startYear">
                 Start Year
               </label>
-              <select className={`${inputCss}`} name="" id="">
+              <select
+                className={`${inputCss}`}
+                name="startYear"
+                id="startYear"
+                value={input.startYear}
+                onChange={(e) => {
+                  setInput({ ...input, startYear: e.target.value });
+                }}
+              >
                 <option className="text-blue-400" value="">
                   Select
                 </option>
-                {year.map((yr, i) => {
-                  return (
-                    <option className="text-blue-400" key={i}>
-                      {yr}
-                    </option>
-                  );
-                })}
+                {year.map((yr, i) => (
+                  <option className="text-blue-400" key={i} value={yr}>
+                    {yr}
+                  </option>
+                ))}
               </select>
             </div>
 
             <div>
-              <label className=" text-sm font-bold" htmlFor="Ending Year ">
+              <label className="text-sm font-bold" htmlFor="endYear">
                 End Year
               </label>
-              <select className={`${inputCss}`} name="" id="">
+              <select
+                className={`${inputCss}`}
+                name="endYear"
+                id="endYear"
+                value={input.endYear}
+                onChange={(e) => {
+                  setInput({ ...input, endYear: e.target.value });
+                }}
+              >
                 <option className="text-blue-400" value="">
                   Select
                 </option>
-                {year.map((yr, i) => {
-                  return (
-                    <option className="text-blue-400" key={i}>
-                      {yr}
-                    </option>
-                  );
-                })}
+                {year.map((yr, i) => (
+                  <option className="text-blue-400" key={i} value={yr}>
+                    {yr}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
-
           <div className="flex justify-center mt-5 gap-5 font-bold text-blue-500">
             <button>Add Item</button>
             <button>Remove Item</button>
           </div>
-<<<<<<< HEAD
-=======
 
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
           <div className="flex justify-end">
             <Link to="/detailfilling/education">
               <button
                 type="submit"
-<<<<<<< HEAD
-=======
                 onClick={addWorkEx}
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
                 className="text-white bg-blue-500 m-2 p-2 rounded-2xl"
               >
                 Next
@@ -168,10 +148,6 @@ function WorkExperience() {
           </div>
         </form>
       </div>
-<<<<<<< HEAD
-      {/* </form> */}
-=======
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
     </div>
   );
 }
--- src/Store/dataStoreSlice.js
+++ src/Store/dataStoreSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 export const dataStoreSlice = createSlice({
   name: "dataStore",
@@ -8,7 +8,8 @@ export const dataStoreSlice = createSlice({
       lastName: "",
       Email: "",
       Mobile: "",
-      Address: "",
+      Address1: "",
+      Address2: "",
       City: "",
       State: "",
       Pin: "",
@@ -16,15 +17,11 @@ export const dataStoreSlice = createSlice({
     },
     workEx: [
       {
-        id: 1,
         title: "",
         orgName: "",
         startYear: "",
-<<<<<<< HEAD
         endYear: "",
-=======
-        endYear: ""
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
+        jobDescription: "",
       },
     ],
     education: [
@@ -46,22 +43,13 @@ export const dataStoreSlice = createSlice({
   reducers: {
     updatePersonalInfo: (state, action) => {
       //this function updates the targeted key of the personalInfo element of dataStore //
-      state.personalInfo = action.payload;
+      state.personalInfo[action.payload.key] = action.payload.value;
     },
 
     updateWorkEx: (state, action) => {
       //this function updates the targeted key of the workEx element of dataStore //
-<<<<<<< HEAD
-      const exp = {
-        id: nanoid(),
-        title: action.payload.title,
-        orgName: action.payload.orgName,
-        startYear: action.payload.startYear,
-        endYear: action.payload.endYear,
-      };
-      state.workEx.push(exp);
-=======
-      state.workEx = action.payload;
+      state.workEx[action.payload.index][action.payload.key] =
+        action.payload.value;
     },
     updateEducation: (state, action) => {
       //this function updates the targeted key of the education element of dataStore //
@@ -76,21 +64,32 @@ export const dataStoreSlice = createSlice({
     updateState: (state, action) => {
       //this function can be called to update any targeted element of dataStore //
       state[action.payload.key] = action.payload.value;
->>>>>>> 68d25688482fa4226cbcfdadc5b3f78aedeead4e
     },
-
-    removeWorkEx: (state, action) => {
-      state.workEx = state.workEx.filter(
-        (workEx) => workEx.id !== action.payload
-      );
+    updateErrorMessages: (state, action) => {
+      //this function updates errorMessages element of dataStore //
+      let key = action.payload.key;
+      if (action.payload.index) {
+        key += "_" + action.payload.index;
+      }
+      state.errorMessages[key] = action.payload.value;
     },
-    updateSkills: (state, action) => {
-      state.skills = action.payload;
+    addArrayElement: (state, action) => {
+      //this function is used to push a blank object in the array of elements(workEx,education& keySkills)
+      //when the user clicks on the Add-new button to add more related details//
+      state[action.payload.key].push(action.payload.element);
+    },
+    removeArrayElement: (state, action) => {
+      //this function deletes the latest saved details in the array of elements(workEx,education& keySkills), when the user clicks on the remove button//
+      state[action.payload.key].pop();
     },
   },
 });
 
-export const { updatePersonalInfo, updateWorkEx, updateSkills } =
-  dataStoreSlice.actions;
+export const {
+  updatePersonalInfo,
+  updateWorkEx,
+  updateEducation,
+  updateSkills,
+} = dataStoreSlice.actions;
 
 export default dataStoreSlice.reducer;
